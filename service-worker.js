0e07e12c80e047f0d86f53266756b785aaf45a16
pedradanapleura <pedradanapleura@gmail.com>
2026-02-27 19:30:49 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,30 @@
-self.addEventListener("install", () => self.skipWaiting());
+const CACHE_NAME = "pedrada-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/logo-192.png",
+  "/logo-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
 
 self.addEventListener("activate", event => {
   event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
