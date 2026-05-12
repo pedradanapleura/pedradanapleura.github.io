ba87703fed5cfd753386d48d1a082ef12da5894b
pedradanapleura <pedradanapleura@gmail.com>
2026-05-12 19:40:07 -0300

    Refactor service worker to remove cache management
    Removed caching logic and simplified fetch event handling.

--- service-worker.js
+++ service-worker.js
@@ -1,44 +1,9 @@
-const CACHE_NAME = "pedrada-cache-v2";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/site.webmanifest",
-  "/android-chrome-192x192.png",
-  "/android-chrome-512x512.png",
-  "/favicon-32x32.png",
-  "/apple-touch-icon.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
+  event.waitUntil(clients.claim());
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", () => {});
